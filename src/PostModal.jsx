87b1ff7e001c500d5b2c47607a7ddcb5e9b92494
jf2024 <>
2021-02-02 14:40:59 -0800

    Finished Blog App

---
+++ src/PostModal.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { Modal, Spinner } from "react-bootstrap";
+
+export default function PostModal(props) {
+  const { postId, show, closePostModal } = props;
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {}, [setPost, postId]);
+  fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    .then((response) => response.json())
+    .then((json) => setPost(json));
+
+  if (!post) {
+    return <Spinner animation="border" />;
+  }
+
+  return (
+    <Modal show={show} onHide={closePostModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>{post.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{post.body}</Modal.Body>
+    </Modal>
+  );
+}
